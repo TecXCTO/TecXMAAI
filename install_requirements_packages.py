af3909e35206f299041e8d53461d3b45dc917025
TecXCTO <cto.tecx@gmail.com>
2026-01-03 18:35:39 +0530

    Update install_requirements_packages.py

--- install_requirements_packages.py
+++ install_requirements_packages.py
@@ -5,29 +5,29 @@ un_installed_list=[]
 for requirement in requirements:
     installed_list.append(f"/n # {requirement}")
     with open(requirement, 'r') as f:
-    for line in f:
-        package = line.strip()
-        if package and not package.startswith('#'):
-            try:
-                subprocess.check_call(['pip', 'install', package])
-                installed_list.append(package)
-                #installed_list.extend(package)
-            except subprocess.CalledProcessError:
-                un_installed_list.append(package)
-                #un_installed_list.extend(package)
-                print(f"Failed to install {package}. Skipping...")
+        for line in f:
+            package = line.strip()
+            if package and not package.startswith('#'):
+                try:
+                    subprocess.check_call(['pip', 'install', package])
+                    installed_list.append(package)
+                    #installed_list.extend(package)
+                except subprocess.CalledProcessError:
+                    un_installed_list.append(package)
+                    #un_installed_list.extend(package)
+                    print(f"Failed to install {package}. Skipping...")
     #with open('requirements_all.txt', 'r') as f:
-    #for line in f:
-        #package = line.strip()
-        #if package and not package.startswith('#'):
-            #try:
-                #subprocess.check_call(['pip', 'install', package])
-                #installed_list.append(package)
-                ####installed_list.extend(package)
-            #except subprocess.CalledProcessError:
-                #print(f"Failed to install {package}. Skipping...")
-                #un_installed_list.append(package)
-                #####un_installed_list.extend(package)
+        #for line in f:
+            #package = line.strip()
+            #if package and not package.startswith('#'):
+                #try:
+                    #subprocess.check_call(['pip', 'install', package])
+                    #installed_list.append(package)
+                    ####installed_list.extend(package)
+                #except subprocess.CalledProcessError:
+                    #print(f"Failed to install {package}. Skipping...")
+                    #un_installed_list.append(package)
+                    #####un_installed_list.extend(package)
 with open('installed_list.txt', 'w') as f:
     #f.writelines([j + '\n' for i,j in installed_list])
     for package_list in installed_list:
