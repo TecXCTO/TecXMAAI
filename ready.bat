e5f1532acd61a02a11747211d4faf092b0e85a5e
TecXCTO <cto.tecx@gmail.com>
2026-01-04 09:35:14 +0530

    Update and rename ready.sh to ready.bat

---
+++ ready.bat
@@ -0,0 +1,13 @@
+python >3.10 -m venv .tecxmaai
+source .tecxmaai/bin/activate
+winget install dvc
+winget install --id Iterative.DVC
+dvc --version
+pip install dvc
+pip install [all]
+
+gh repo clone TecXCTO/TecXMAAI
+cd TecXMAAI
+git init
+dvc init
+git commit -m "Initialize DVC"
--- ready.sh
+++
@@ -1,7 +0,0 @@
-python >3.10 -m venv .tecxmaai
-source .tecxmaai/bin/activate
-windget install dvc
-gh repo clone TecXCTO/TecXMAAI
-cd TecXMAAI
-git init
-dvc init
