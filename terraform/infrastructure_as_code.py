7b8b87989891a238ca39401bc2d8ffa017a2d988
TecXCTO <cto.tecx@gmail.com>
2026-01-07 09:23:21 +0530

    Update infrastructure_as_code.py

--- terraform/infrastructure_as_code.py
+++ terraform/infrastructure_as_code.py
@@ -1,4 +1,5 @@
 '''
-Infrastructure as Code ( terraform/ or
-crossplane/):Define the high-performance computing (HPC) or cloud environment where the model runs.
+Infrastructure as Code ( terraform/ or crossplane/):
+
+Define the high-performance computing (HPC) or cloud environment where the model runs.
 '''
