9c52e45dae042568e22d4e2e2a608be5a20a659c
TecXCTO <cto.tecx@gmail.com>
2026-01-11 08:44:49 +0530

    Update mech.bat

--- install/api/bat/mech.bat
+++ install/api/bat/mech.bat
@@ -1,5 +1,17 @@
+# 1. Primary Installation Command
+
 # Upgrade pip first
 python -m pip install -U pip
 
 # Install the full PyAnsys suite
 python -m pip install pyansys[all]
+
+# 2. Specialized AI & Digital Twin Libraries
+
+# Ansys SimAI: 
+python -m pip install ansys-simai-core
+# Ansys TwinAI: 
+python -m pip install pytwin
+# Data Processing (DPF): 
+python -m pip install ansys-dpf-core[graphics]
+
