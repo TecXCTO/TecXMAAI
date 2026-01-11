24d2e2c8ea20b1c6f1a302d21874985cf01267d7
TecXCTO <cto.tecx@gmail.com>
2026-01-11 09:10:34 +0530

    Update mech.bat

--- install/api/bat/mech.bat
+++ install/api/bat/mech.bat
@@ -25,9 +25,25 @@ pip install pywin32
 # Optional community-based Python wrapper (verify latest 2026 compatibility)
 pip install pyinventor
 
+#
+# 1. Simulation & Physics (PyAnsys)
+
+# Complete Suite:
+python -m pip install -U pip
+python -m pip install pyansys[all]
+
+# Targeted Solvers (Fluid or Structural):
+# For Fluid Dynamics agents
+python -m pip install pyansys[fluent-all]
+
+# For Structural Analysis agents
+python -m pip install ansys-mapdl-core
 
 
+# 2. Cloud CAD & Generative Design (Autodesk)
 
+# APS Python Toolkit:
+python -m pip install aps-toolkit
 
-
-```
+# Design Automation Utilities:
+dotnet add package Autodesk.Forge.DesignAutomation.Inventor.Utils
